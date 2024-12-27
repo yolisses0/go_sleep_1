3a11574efb3f227bca673da3c346c90b843f2ad8
Yolisses <iamurise@gmail.com>
2024-12-27 05:57:08 -0300

    Remove random "This is: " output

--- linux/runner/main.cc
+++ linux/runner/main.cc
@@ -5,8 +5,7 @@
 
 int main(int argc, char **argv)
 {
-  std::cout << "This is: \n"
-            << b::embed<"test.txt">() << std::endl;
+  std::cout << b::embed<"test.txt">() << std::endl;
   g_autoptr(MyApplication) app = my_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
 }
